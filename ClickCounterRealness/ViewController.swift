4eae74fee3a5ac395395cbb52ff21bfb7821182e
Meredith Bayne <mrdthbyn@gmail.com>
2018-10-25 16:22:59 -0700

    stub viewcontroller button and label

--- ClickCounterRealness/ViewController.swift
+++ ClickCounterRealness/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.purple, for: .normal)
+        view.addSubview(button)
     }
 
 
